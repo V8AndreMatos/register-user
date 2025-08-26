88ca3e94df564ea60abf7f7c314f6ef9b5fe9aff
Andre <andreluisv8@gmail.com>
2025-08-25 22:51:49 -0300

    Created class User

---
+++ src/main/java/com/register/user/entity/User.java
@@ -0,0 +1,62 @@
+package com.register.user.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Integer idade;
+
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome, Integer idade, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
